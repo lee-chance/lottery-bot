f7f6d29d6db6d274f508d21909a2225255a8cc89
lee-chance <46893726+lee-chance@users.noreply.github.com>
2023-09-22 02:12:02 +0900

    Update controller.py

--- controller.py
+++ controller.py
@@ -56,10 +56,16 @@ def check():
     globalAuthCtrl = auth.AuthController()
     globalAuthCtrl.login(username, password)
     response = check_winning_lotto645(globalAuthCtrl)
-    send_message(0, 0, response=response, webhook_url=discord_webhook_url)
+    if slack_webhook_url != '':
+        send_message(0, 0, response=response, webhook_url=slack_webhook_url)
+    if discord_webhook_url != '':
+        send_message(0, 0, response=response, webhook_url=discord_webhook_url)
 
     response = check_winning_win720(globalAuthCtrl)
-    send_message(0, 1, response=response, webhook_url=discord_webhook_url)
+    if slack_webhook_url != '':
+        send_message(0, 1, response=response, webhook_url=slack_webhook_url)
+    if discord_webhook_url != '':
+        send_message(0, 1, response=response, webhook_url=discord_webhook_url)
 
 def buy(): 
     
